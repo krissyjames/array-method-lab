396537a8f3d5cbfa62036a598922d9f44193b186
krissyjames <kristina.ashley.cook@gmail.com>
2024-09-30 17:55:19 -0400

    completion of findAboveFreezing test

--- __tests__/arrayMethods.test.js
+++ __tests__/arrayMethods.test.js
@@ -5,7 +5,7 @@ const {
     arrayHasValue,
     getLastIndexOf,
     // getNumberOfTimes,
-    // findAboveFreezing,
+    findAboveFreezing,
     // returnString,
     // sortArrayBasedOnNumber,
     // concatArrays,
@@ -68,11 +68,11 @@ describe('JavaScript Array Methods Tests', () => {
     // });
 
     // // Test for findAboveFreezing
-    // test('findAboveFreezing should return the first temperature above freezing', () => {
-    //     expect(findAboveFreezing([30, 32, 35, 40])).toBe(35);
-    //     expect(findAboveFreezing([20, 10, 5])).toBe(undefined);
-    //     expect(findAboveFreezing('not an array')).toBe('Invalid input');
-    // });
+    test('findAboveFreezing should return the first temperature above freezing', () => {
+        expect(findAboveFreezing([30, 32, 35, 40])).toBe(35);
+        expect(findAboveFreezing([20, 10, 5])).toBe(undefined);
+        expect(findAboveFreezing('not an array')).toBe('Invalid input');
+    });
 
     // // Test for returnString
     // test('returnString should return the array as a string with an ampersand as the separator', () => {
